a5f1f9c140debeb1f7743729f1b4fe9aa492de39
Anastasia Bielievitina <abielievitina@gmail.com>
2024-05-16 19:13:27 -0700

    issue

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { useConvexAuth, useMutation } from "convex/react";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import NewUserPlaceholder from "../../_components/new-user-placeholder";
 
-export default function DocumentPage(): JSX.Element {
+export default function Page() {
   const router = useRouter();
   const createDocument = useMutation(api.documents.createDocument);
   const { isAuthenticated, isLoading } = useConvexAuth();
   if (!isAuthenticated && isLoading) {
-    return redirect("/");
+    return router.push("/");
   }
   const onCreateDocument = () => {
     const promise = createDocument({ title: "Untitled" }).then((documentId) =>
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,16 +3,16 @@
 import SearchModal from "@/components/modals/search-modal";
 import SettingsModal from "@/components/modals/settings-modal";
 import { useConvexAuth } from "convex/react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import Sidebar from "./_components/sidebar";
 import { EdgeStoreProvider } from "@/lib/edgestore";
-import Navbar from "./(routes)/documents/[documentId]/_components/navbar";
 import { WidthProvider } from "@/contexts/WidthContext";
 
 const MainLayoyt = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter()
   const { isAuthenticated } = useConvexAuth();
   if (!isAuthenticated) {
-    return redirect("/");
+    return router.push("/documents");
   }
   return (
     <>
