c44551cf94913eb3bf5da99f3aeb7cb59868758f
Anastasia Bielievitina <abielievitina@gmail.com>
2024-05-16 18:57:38 -0700

    latest

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,18 +2,16 @@
 
 import { useConvexAuth, useMutation } from "convex/react";
 import { redirect, useRouter } from "next/navigation";
-import { useUser } from "@clerk/clerk-react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import NewUserPlaceholder from "../../_components/new-user-placeholder";
 
-export default function Home() {
-  const { user } = useUser();
+export default function DocumentPage() {
   const router = useRouter();
   const createDocument = useMutation(api.documents.createDocument);
   const { isAuthenticated, isLoading } = useConvexAuth();
   if (!isAuthenticated && isLoading) {
-    return redirect("/");
+    return null;
   }
   const onCreateDocument = () => {
     const promise = createDocument({ title: "Untitled" }).then((documentId) =>
